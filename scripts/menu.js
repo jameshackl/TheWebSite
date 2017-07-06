2d993b7214626e0ab082adb11db504803d22242c
jameshackl <james.f.hackl@gmail.com>
2017-07-06 01:51:08 -0600

    centered menu and further styling

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,6 @@
-document.getElementById("menu-container").innerHTML = '<a href="./about.html"> about</a>' +
-            '<a href="./contact.html"> contact</a>' +
-            '<a href="./portfolio.html"> portfolio</a>' +
-            '<a href="./resume.html"> resume</a>'
+document.getElementById("menu-container").innerHTML = '<div class="menu"><a href="./index.html">home</a>' +
+            '<a href="./about.html">about</a>' +
+            '<a href="./contact.html">contact</a>' +
+            '<a href="./portfolio.html">portfolio</a>' +
+            '<a href="./resume.html">resume</a> </div>' +
+            '<hr>'
