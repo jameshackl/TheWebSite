4f6af89eb56f7e56359fe40ceed015081b56e1a5
jameshackl <james.f.hackl@gmail.com>
2017-07-06 02:08:26 -0600

    restructured the menu offerings

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,6 @@
-document.getElementById("menu-container").innerHTML = '<div class="menu"><a href="./index.html">home</a>' +
-            '<a href="./about.html">about</a>' +
-            '<a href="./contact.html">contact</a>' +
+document.getElementById("menu-container").innerHTML = '<div class="menu"><a href="./index.html">about</a>' +
             '<a href="./portfolio.html">portfolio</a>' +
+            '<a href="./hobby.html">hobby projects</a>' +
             '<a href="./resume.html">resume</a> </div>' +
+            '<a href="./contact.html">contact</a>' +
             '<hr>'
