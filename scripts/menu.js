f413adb0e14a8a52fb7f43863a7b4426d94c65da
jameshackl <james.f.hackl@gmail.com>
2017-07-05 23:56:15 -0600

    more ground work and javascript menu provider implemented

---
+++ scripts/menu.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-container").innerHTML = '<a href="./about.html"> about</a>' +
+            '<a href="./contact.html"> contact</a>' +
+            '<a href="./portfolio.html"> portfolio</a>' +
+            '<a href="./resume.html"> resume</a>'
