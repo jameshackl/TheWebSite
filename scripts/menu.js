876066196360ef09569a989fa3f9c60c58c0f5d9
jameshackl <james.f.hackl@gmail.com>
2017-07-18 03:04:43 -0600

    removed resume page

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,5 @@
-document.getElementById("menu-container").innerHTML = '<div class="menu"><a href="./index.html">about</a>' +
-            '<a href="./portfolio.html">portfolio</a>' +
-            '<a href="./hobby.html">hobby projects</a>' +
-            '<a href="./resume.html">resume</a>' +
-            '<a href="./contact.html">contact</a> </div>' +
+document.getElementById("menu-container").innerHTML = '<div class="menu"><a href="./index.html" class="menu-item">about</a>' +
+            '<a href="./portfolio.html" class="menu-item">portfolio</a>' +
+            '<a href="./hobby.html" class="menu-item">hobby projects</a>' +
+            '<a href="./contact.html" class="menu-item">contact</a> </div>' +
             '<hr>'
